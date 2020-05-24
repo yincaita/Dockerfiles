bf78e3ab041e1ece923317dfa3b406833cb5e0dd
yincaiTA <ls1229344939@163.com>
2020-05-24 15:54:40 +0800

    commit 编译c文件

---
+++ yunjisuan/helloworld.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+ 
+int main() {
+
+	FILE *in= fopen("helloworld.c", "r");
+	char buf[1024];
+
+	while (fgets(buf, sizeof(buf), in) != NULL) {
+		printf("%s", buf);
+	}
+
+	fclose(in);
+
+	printf("\n–’√˚: ¡ı…∆\n—ß∫≈: 2018051199\n");
+	return 0;
+}
